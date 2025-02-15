46c82f5872b32f1eec3a26a5523981df139f553f
sartroy24 <sarthakroy10@gmail.com>
2025-02-15 14:29:43 +1100

    Car is running now

--- HelloWord/cargame.py
+++ HelloWord/cargame.py
@@ -1,1 +1,11 @@
-#test
+command = ""
+while command != "quit":
+    command = input("Enter the command:")
+    if command.lower() == "start":
+        print("Your car has started")
+    elif command.lower() == "stop":
+        print("Your car has stopped")
+    elif command.lower() == "quit":
+        break
+    else:
+        print("Wrong command")
