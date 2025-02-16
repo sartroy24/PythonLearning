e9f3fe7a4b099326efe33b886cfa4713ebd6b6ec
sartroy24 <sartroy2022@gmail.com>
2025-02-17 01:13:51 +1100

    emoji converter, functions, exception handling and dictionaries

---
+++ HelloWord/functions.py
@@ -0,0 +1,12 @@
+def greet_user(first_name, last_name):
+    print("Welcome")
+    print(f'Hi {first_name} {last_name}')
+
+
+greet_user("Sarthak", "Roy")
+
+
+def square(number):
+    return number * number
+
+print(square(number = 3))
