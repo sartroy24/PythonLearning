4287f2ad15673608e9324bf94864489d85f41f61
sartroy24 <sartroy2022@gmail.com>
2025-02-18 00:52:16 +1100

    Latest changes before using pycharm

--- HelloWord/app.py
+++ HelloWord/app.py
@@ -1,14 +1,5 @@
-secret_number = 9
-guess_no = 1
-guess_limit = 3
-while guess_no <= guess_limit:
-    input_no = int(input("Make a guess: "))
-    guess_no += 1
-    if input_no == secret_number:
-        print("Correct guess")
-        break
-    elif guess_no < guess_limit:
-        print("Wrong guess, please try again")
-        continue
-    else:
-        print("Sorry please try again")
+import emojiconvfunction
+from emojiconvfunction import emoji_converter
+
+# emojiconvfunction.emoji_converter("Good morning sunshine :(")
+emoji_converter("Good morning sunshine")
