37154242e3414fb06618d109cd9f9f942ef8f711
sartroy24 <sartroy2022@gmail.com>
2025-02-22 00:21:10 +1100

    Making changes from pycharm again

--- HelloWord/app.py
+++ HelloWord/app.py
@@ -2,9 +2,12 @@ import emojiconvfunction
 from  ecommerce.shipping import calculate_shipping, confirm_shipping
 from emojiconvfunction import emoji_converter
 from randomdicetuple import roll
+
 #emojiconvfunction.emoji_converter("Good morning sunshine :(")
 #print(emoji_converter("Good morning sunshine :("))
 #calculate_shipping()
 #confirm_shipping()
 
 print(roll())
+
+
