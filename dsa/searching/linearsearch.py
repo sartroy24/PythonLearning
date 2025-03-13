df0cc08ffd48cd72a66bf4f29ad04f6942f6512f
sartroy24 <sartroy2022@gmail.com>
2025-03-13 18:38:20 +1100

    binary search and linear search deployed

---
+++ dsa/searching/linearsearch.py
@@ -0,0 +1,17 @@
+def linearSearch(arr, searchValue):
+    for i in range(len(arr)):
+        if arr[i] == searchValue:
+            return i
+
+    return -1
+
+
+arr = [3, 7, 2, 9, 5]
+targetVal = 9
+
+result = linearSearch(arr, targetVal)
+
+if result != -1:
+    print("Value",targetVal,"found at index",result)
+else:
+    print("Value",targetVal,"not found")
